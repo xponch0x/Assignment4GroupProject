99ba728ef88625ccd2dc7b54b3e6b367203d831f
Griffin Winstead <grifwin03@gmail.com>
2023-11-13 13:10:53 -0600

    Created logon page (broken still) and fixed logon hyperlinks

--- Logon/Logon.aspx
+++ Logon/Logon.aspx
@@ -1,3 +1,14 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Logon.aspx.cs" Inherits="Assignment4GroupProject.Logon.Logon" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Login ID="Login1" runat="server" BackColor="#EFF3FB" BorderColor="#B5C7DE" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" Height="248px" OnAuthenticate="Login1_Authenticate" Width="398px" DestinationPageUrl="~/About.aspx" style="margin-top: 0px">
+    <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
+    <LoginButtonStyle BackColor="White" BorderColor="#507CD1" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#284E98" />
+    <TextBoxStyle Font-Size="0.8em" />
+    <TitleTextStyle BackColor="#507CD1" Font-Bold="True" Font-Size="0.9em" ForeColor="White" />
+</asp:Login>
+<br />
+<br />
+<br />
+<asp:TextBox ID="TextBox1" runat="server" Width="190px"></asp:TextBox>
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
 </asp:Content>
--- Logon/Logon.aspx.cs
+++ Logon/Logon.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +10,84 @@ namespace Assignment4GroupProject.Logon
 {
     public partial class Logon : System.Web.UI.Page
     {
+
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\grifw\\source\\repos\\Assignment4GroupProject\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
+        KarateDataContext dbcon;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string userName = Login1.UserName;
+            string password = Login1.Password;
+
+            try
+            {
+                dbcon = new KarateDataContext(conn);
+
+                var names = from name in dbcon.NetUsers
+                            where name.UserName == userName
+
+                            select new
+                            {
+                                name.UserName
+                            };
+
+                var pswds = from pswd in dbcon.NetUsers
+                            where pswd.UserPassword == password
+
+                            select new
+                            {
+                                pswd.UserPassword
+                            };
+
+                string username1 = names.First().ToString();
+                string password1 = pswds.First().ToString();
+
+                var userType = from x in dbcon.NetUsers
+                         where x.UserName == userName
+                         select new
+                         {
+                             x.UserType
+                         };
+
+                string uType = userType.First().ToString();
+
+
+                if (userName == username1 && password == password1)
+                {
+                    if(uType == "Member")
+                    {
+                        Login1.DestinationPageUrl = "Members.aspx";
+                    }
+                    if (uType == "Instructor")
+                    {
+                        Login1.DestinationPageUrl = "Instructors.aspx";
+                    }
+
+
+                    FormsAuthentication.RedirectFromLoginPage(userName, true);
+                    
+                }
+                else
+                {
+                    Response.Redirect("Logon.aspx", true);
+                    TextBox1.Text = "ahsdahsdas";
+                    Label1.Text = "apsofihadf";
+                }
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+            }
         }
     }
 }
